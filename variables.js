a3bacd910c36aa191ce733a919000cc839e5cc72
Crs Snz <82622310+ecristinasanchez@users.noreply.github.com>
2021-04-20 20:54:52 -0500

    RetoTecnoLatina

---
+++ variables.js
@@ -0,0 +1,25 @@
+//STRING - Texto plano
+let comidaMexicana = "Tacos dorados";
+
+//NUMBER - Número
+let tacosQueMeComo = 8;
+
+//BOOLEANO - Valor vinculado al verdadero o falso
+let sonSabrosos = true;
+let sabenFeo = false;
+
+//UNDEFINED - Valor que tiene una variable a la cual no se le ha asignado una variable (aún)
+let fechaDeComida = undefined;
+
+//NULL - La variable no tiene ningún valor. Puede referirse a un No aplica
+let tacosSinGuacamole = null;
+
+//ARREGLO - Conjunto de datos. Cada lugar en el arreglo puede ser otro tipo de dato
+let comidaMexicana = ["Tacos", "Enchiladas", "Pozole"]
+
+//OBJETO - Conjunto de datos que se estructura en un formato llave:valor
+let nocheMexicana = {
+    día: "15",
+    mes: "septiembre",
+    PuedecambiarseLaFecha: "false"
+}
